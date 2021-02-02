f79e175295c6e6332dc8d157559d25b570cd2a9a
ysaddiq <36053139+ysaddiq@users.noreply.github.com>
2021-02-02 22:46:58 +0100

    Create roles.tf

---
+++ environments/test/roles.tf
@@ -0,0 +1,19 @@
+# google_project_iam_custom_role.dialogflow_gia_client:
+resource "google_project_iam_custom_role" "dialogflow_gia_client" {
+    description = "Based on: Dialogflow API Admin"
+    permissions = [
+        "dialogflow.agents.get",
+        "dialogflow.entityTypes.get",
+        "dialogflow.entityTypes.list",
+        "dialogflow.intents.get",
+        "dialogflow.intents.list",
+        "dialogflow.sessionEntityTypes.get",
+        "dialogflow.sessionEntityTypes.list",
+        "dialogflow.sessions.detectIntent",
+        "dialogflow.sessions.streamingDetectIntent",
+    ]
+    project = "${var.project}"
+    role_id     = "dialogflow.giaclient"
+    stage       = "GA"
+    title       = "Dialogflow API GIA Client"
+}
